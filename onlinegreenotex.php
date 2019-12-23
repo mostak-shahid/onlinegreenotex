55125bdb52c47c7353fc7768bf8e3d14f18a7fcb
Md. Mostak Shahid <mostak.shahid@gmail.com>
2019-12-23 11:44:10 +0600

    Initial commit

---
+++ onlinegreenotex.php
@@ -0,0 +1,9 @@
+<?php
+/*
+Plugin Name: Online Greenotex
+Plugin URI: http://wordpress.org/plugins/hello-dolly/
+Description: This is a plugin for Online Greenotex.
+Author: Md. Mostak Shahid
+Version: 1.0.0
+Author URI: http://www.mdmostakshahid.me
+*/
